d386db333b0ed3ca819035b4cdb8b41bf5197c1b
Joe McGarry <joewmcgarry@gmail.com>
2022-09-12 16:59:41 +0100

    Begins testing adding to transaction history, date mocking needs fixed

--- bank_account.py
+++ bank_account.py
@@ -1,8 +1,11 @@
+import datetime
+
+
 class BankAccount:
     def __init__(self):
         self.balance = 0
         self.transactions = []
-    
+
     def get_balance(self):
         return self.balance
 
@@ -11,6 +14,16 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
-    
+        date = self.__get_time()
+        self.transactions.append({
+            "Date": date,
+            "Credit": amount,
+            "Debit": None,
+            "Balance": self.balance
+        })
+
     def withdraw(self, amount):
         self.balance -= amount
+
+    def __get_time(self):
+        return datetime.datetime.now().date()
--- bank_account_test.py
+++ bank_account_test.py
@@ -1,5 +1,6 @@
 from bank_account import *
-import unittest
+from unittest.mock import MagicMock
+from datetime import datetime
 
 
 def test_initial_account():
@@ -12,8 +13,18 @@ def test_initial_account():
 def test_depositing_100():
     account = BankAccount()
     account.deposit(100)
+    mock_date = MagicMock(return_value = datetime(2022, 9, 10))
+    transaction = {
+        "Date": datetime(2022, 9, 10),
+        "Credit": 100,
+        "Debit": None,
+        "Balance": 100
+    }
+    statement = account.get_statement()
     assert type(account) == BankAccount
     assert (account.get_balance()) == 100
+    assert transaction in statement
+    assert len(statement) == 1
 
 
 def test_withdrawal_of_50():
