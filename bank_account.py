217024278f6ab4fc3e83050e08edef5475b6c9f2
Joe McGarry <joewmcgarry@gmail.com>
2022-09-12 16:15:06 +0100

    Adds second testing for transaction history initially

--- bank_account.py
+++ bank_account.py
@@ -1,6 +1,10 @@
 class BankAccount:
     def __init__(self):
         self.balance = 0
+        self.transactions = []
     
     def get_balance(self):
         return self.balance
+
+    def get_statement(self):
+        return self.transactions
--- bank_account_test.py
+++ bank_account_test.py
@@ -5,3 +5,4 @@ def test_initial_account():
     account = BankAccount()
     assert type(account) == BankAccount
     assert(account.get_balance()) == 0
+    assert(account.get_statement()) == []
